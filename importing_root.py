a0f7d5350adff45943f0f1b4c1c53f754e47c7a4
[nolanwu1112] <nolanwu1112@gmail.com>
2017-08-06 21:58:58 +0000

    Modified importing_root.py

--- importing_root.py
+++ importing_root.py
@@ -1,5 +1,15 @@
 # -*- utf-8 -*-
+""" todo
+1. searching for uppercase words and surround it with {{c1: :root}}
+2. searching for words within quotations and surround it with {{cn: }}
+3. adding <hr> after first line
+4. make everything into one line"""
 
 import re
 
+def main():
+    """ main function"""
+
+    with open(r"root.txt", 'r') as file:
+        content_list = file.readlines()
 
